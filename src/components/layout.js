8c5fdcc70190252eb44dbd466c59cabf752fc028
KHCode <hillkri@oregonstate.edu>
2021-01-28 14:23:00 -0800

    removed footer

--- src/components/layout.js
+++ src/components/layout.js
@@ -39,11 +39,6 @@ const Layout = ({ children }) => {
         <StyledContent>
           <Nav />
             <div>{children}</div>
-            <footer>
-              © {new Date().getFullYear()}, Built with
-              {` `}
-              <a href="https://www.gatsbyjs.org">Gatsby</a>
-            </footer>
         </StyledContent>
         
       </div>
